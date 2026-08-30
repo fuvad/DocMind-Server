a792d69e0fa59c84873af98fb89474d8ddc48681
Fuvad <fuvad777@gmail.com>
2026-08-30 12:12:46 +0530

    Added Auth using Clerk and also added Project APIs

---
+++ auth.py
@@ -0,0 +1,42 @@
+"""
+This verifies and extracts current user's clerk_id
+Adds something like a middleware that runs on every single api call
+"""
+
+
+import os
+from fastapi import Request, HTTPException
+from clerk_backend_api import AuthenticateRequestOptions, Clerk
+
+# Initialize Clerk client
+clerk_client = Clerk(bearer_auth=os.getenv('CLERK_SECRET_KEY'))
+
+ALLOWED_ORIGINS = os.getenv('ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+
+# This func recieves api request and responds clerk_id
+async def get_current_user(request: Request) -> str: 
+    try:
+        request_state = clerk_client.authenticate_request(
+            request,
+            AuthenticateRequestOptions(
+                authorized_parties=["http://localhost:3000"]    # Limits the domains that can access this endpoint
+            )
+        )       # Tells us whether the token is valid or not
+        
+        if not request_state.is_signed_in:
+            raise HTTPException(status_code=401, detail="Not authenticated")
+        
+        clerk_id = request_state.payload.get("sub")
+        
+        if not clerk_id:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        
+        return clerk_id
+        
+    except Exception as e:
+        raise HTTPException(
+            status_code=401, 
+            detail=f"Authentication failed: {str(e)}"
+        ) 
+
+
