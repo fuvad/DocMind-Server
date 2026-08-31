72e46e806c5bc176ec5a80f25ae0d9fed565c370
Fuvad <fuvad777@gmail.com>
2026-09-01 00:33:12 +0530

    Improved Projects Route

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
-from routers import users, projects
+from routers import users, projects, files, chats
 
 load_dotenv()
 
@@ -25,6 +25,8 @@ app.add_middleware(
 
 app.include_router(users.router)
 app.include_router(projects.router) 
+app.include_router(files.router) 
+app.include_router(chats.router) 
 
 @app.get("/")
 async def root():
