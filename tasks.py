aa1e372a3b52c2b247aada13312a364a8ec700ca
Fuvad <fuvad777@gmail.com>
2026-09-02 21:48:57 +0530

    Add document downloading and partitioning for PDF files

--- tasks.py
+++ tasks.py
@@ -1,6 +1,10 @@
 from celery import Celery
-from database import supabase
+from database import supabase, s3_client, BUCKET_NAME
 import time
+from unstructured.partition.pdf import partition_pdf
+from unstructured.partition.docx import partition_docx
+from unstructured.partition.html import partition_html
+import os
 
 # Create Celery App
 celery_app = Celery(
@@ -12,27 +16,82 @@ celery_app = Celery(
 @celery_app.task
 def process_document(document_id: str):
     """
-        simple test task
+        Real document Processing
     """
+
+    try: 
+
+        doc_result = supabase.table("project_documents").select("*").eq("id", document_id).execute()
+        document = doc_result.data[0]
+
+        # step 1: Download and partition 
+        elements = download_and_partition(document_id, document)
+
+        tables = sum(1 for e in elements if e.category == "Table")
+        images = sum(1 for e in elements if e.category == "Image")
+        text_elements = sum(1 for e in elements if e.category in ["NarrativeText", "Title", "Text"])
+        print(f"📊 Extracted: {tables} tables, {images} images, {text_elements} text elements")
+
+
+        # step 2: Chunk elements 
+
+        #3 Step 3: Summarising chunks 
+
+        #4 Step 4: Vectorization & storing 
+        
     
-    # step 1: Update status to "processing"
-    supabase.table("project_documents").update({
-        "processing_status": "processing"
-    }).eq("id", document_id).execute()
+
+        return {
+            "status": "success", 
+            "document_id": document_id
+        }
+
+    except Exception as e: 
+        pass
     
-    print(f"Processing document {document_id}")
+def download_and_partition(document_id: str, document: dict):
+    """ Download document from S3 / Crawl URL and partition into elements  """
     
-    # Step 2: Simulate actual work (partitioning, chunking,...)
-    time.sleep(5)    # In real implementation, this is where we will process the document
-    
-    # Step 3: Update status to "completed"
-    supabase.table("project_documents").update({
-            "processing_status": "completed"
-    }).eq("id", document_id).execute()
-    
-    print(f"Celery task completed for document: {document_id}")
-    
-    return {
-        "status": "success",
-        "document_id": document_id
-    }   # not necessary, coz it'll be stored in redis, which won't use
+    print(f"Downloading and partitioning document {document_id}")
+
+    source_type = document.get("source_type", "file")
+
+    if source_type == "url":
+        # Crawl URL 
+        pass
+
+    else:
+        # Handle file processing
+        
+        # First download the file from s3
+        s3_key = document["s3_key"]
+        filename = document["filename"]
+        file_type = filename.split(".")[-1].lower()
+
+        #  Download to a temporary location 
+        temp_file = f"/tmp/{document_id}.{file_type}"    # tmp sits at the very root of our computer
+        s3_client.download_file(BUCKET_NAME, s3_key, temp_file)
+
+        elements = partition_document(temp_file, file_type, source_type="file")
+
+
+    os.remove(temp_file)    # after partitioning temp_file is not needed
+
+    return elements
+
+
+def partition_document(temp_file: str, file_type: str, source_type: str = "file"):
+    """ Partition document based on file type and source type """
+
+    if source_type == "url": 
+        pass
+
+    if file_type == "pdf":
+        return partition_pdf(
+            filename=temp_file,  # Path to your PDF file
+            strategy="hi_res", # Use the most accurate (but slower) processing method of extraction
+            infer_table_structure=True, # Keep tables as structured HTML, not jumbled text
+            extract_image_block_types=["Image"], # Grab images found in the PDF
+            extract_image_block_to_payload=True # Store images as base64 data you can actually use
+        )
+
